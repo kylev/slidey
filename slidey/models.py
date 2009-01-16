a3bc43ee290b80d0f1832101b44d23ef0fdcf47b
Kyle VanderBeek <kylev@kylev.com>
2009-01-16 11:22:29 -0800

    Get the basic elements of the db up

--- slidey/models.py
+++ slidey/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class DisplayItem(models.Model):
+    item_name = models.CharField(max_length=40, unique=True)
+    url = models.URLField()
+
+
+class SlideShow(models.Model):
+    show_name = models.CharField(max_length=40, unique=True)
