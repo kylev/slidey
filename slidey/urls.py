9cb3ef35ea089fe1f189130bf57543e58fcb57aa
Kyle VanderBeek <kylev@kylev.com>
2009-01-16 13:37:00 -0800

    Annoying hack to get app-relative static content served.

--- slidey/urls.py
+++ slidey/urls.py
@@ -1,11 +1,17 @@
-from django.conf.urls.defaults import *
+import os
 
+import django.views.static
+from django.conf.urls.defaults import *
+from django.core.urlresolvers import RegexURLPattern
+
+
+APP_DIR = os.path.dirname(os.path.normpath(__file__))
 
 urlpatterns = patterns(
     'slideshow.slidey.views',
     (r'^$', 'index'),
-    # TODO static serve
-    #(r'site_media/(?P<path>.*)$', 'django.views.static.serve',
-    # {
+    # Annoying hack to serve static content from this app dir
+    RegexURLPattern(r'^static/(?P<path>.*)$', django.views.static.serve,
+                    {'document_root': APP_DIR + '/static'}),
     (r'(?P<show_id>\d+)/$', 'show'),
     )
