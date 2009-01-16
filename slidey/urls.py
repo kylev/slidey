64a3aed1a076bd2ac31874fc1fc84bdff74e8bf6
Kyle VanderBeek <kylev@kylev.com>
2009-01-16 12:54:50 -0800

    Start working on template heirarchy.

--- slidey/urls.py
+++ slidey/urls.py
@@ -4,5 +4,8 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns(
     'slideshow.slidey.views',
     (r'^$', 'index'),
+    # TODO static serve
+    #(r'site_media/(?P<path>.*)$', 'django.views.static.serve',
+    # {
     (r'(?P<show_id>\d+)/$', 'show'),
     )
