23442bc5000f0212c5cbefcfe33fb334f5b1cf50
Kyle VanderBeek <kylev@kylev.com>
2009-02-02 15:37:33 -0800

    Start making the show list selectable.

--- slidey/urls.py
+++ slidey/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns(
     (r'^show_control/(?P<show_id>\d+)/$', 'show_control', None, 'slidey-control'),
     (r'^login/$', 'do_login', None, 'slidey-login'),
     (r'^manage/$', 'manage', None, 'slidey-manage'),
+    (r'^show_contents/(?P<show_id>\d+)/$', 'show_contents', None, 'slidey-showcont'),
     (r'^edit/(?P<show_id>\d+)/$', 'edit', None, 'slidey-edit'),
     )
